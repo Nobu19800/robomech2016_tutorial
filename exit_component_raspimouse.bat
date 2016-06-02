83b4505c9d40443972664950b3ac6c78fe2cbd07
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2016-06-02 18:06:31 +0900

    1806

--- exit_component_raspimouse.bat
+++ exit_component_raspimouse.bat
@@ -1,4 +1,5 @@
-cd /d %~dp0
+cd /d %~dp
+set PATH=C:\Python27;C:\Python27\Scripts;%PATH%
 rtexit /localhost/FloatSeqToVelocity0.rtc
 rtexit /localhost/TkJoyStick0.rtc
 rtexit /localhost/RaspberryPiMouseGUI0.rtc
